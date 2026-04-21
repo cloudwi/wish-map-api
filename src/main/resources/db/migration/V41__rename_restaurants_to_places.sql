21e86969a925dda785b716e5a04921a5cc9eeb6e
cloudwi <cloudwi@cloudwiui-MacBookPro.local>
2026-04-21 13:55:50 +0900

    fix: V41에서 이미 drop된 bookmarks 테이블 참조 제거
    V5에서 drop된 bookmarks 테이블을 V41이 rename하려고 시도해 신규 DB 마이그레이션이 실패. bookmarks는 더 이상 도메인에서 사용되지 않으므로 관련 구문 3줄 제거.

--- src/main/resources/db/migration/V41__rename_restaurants_to_places.sql
+++ src/main/resources/db/migration/V41__rename_restaurants_to_places.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS restaurant_images;
 
 ALTER TABLE restaurants RENAME TO places;
 
-ALTER TABLE bookmarks RENAME COLUMN restaurant_id TO place_id;
 ALTER TABLE comments RENAME COLUMN restaurant_id TO place_id;
 ALTER TABLE visits RENAME COLUMN restaurant_id TO place_id;
 ALTER TABLE lunch_vote_candidates RENAME COLUMN restaurant_id TO place_id;
@@ -16,14 +15,12 @@ ALTER INDEX idx_restaurants_place_category RENAME TO idx_places_place_category;
 ALTER INDEX idx_restaurants_price_range RENAME TO idx_places_price_range;
 ALTER INDEX idx_restaurants_suggested_by RENAME TO idx_places_suggested_by;
 
-ALTER INDEX bookmarks_restaurant_id_user_id_key RENAME TO bookmarks_place_id_user_id_key;
 ALTER INDEX idx_comments_restaurant RENAME TO idx_comments_place;
 ALTER INDEX idx_visits_restaurant RENAME TO idx_visits_place;
 ALTER INDEX idx_visits_restaurant_created RENAME TO idx_visits_place_created;
 ALTER INDEX idx_visits_restaurant_user_date RENAME TO idx_visits_place_user_date;
 ALTER INDEX lunch_vote_candidates_vote_id_restaurant_id_key RENAME TO lunch_vote_candidates_vote_id_place_id_key;
 
-ALTER TABLE bookmarks RENAME CONSTRAINT bookmarks_restaurant_id_fkey TO bookmarks_place_id_fkey;
 ALTER TABLE comments RENAME CONSTRAINT comments_restaurant_id_fkey TO comments_place_id_fkey;
 ALTER TABLE visits RENAME CONSTRAINT visits_restaurant_id_fkey TO visits_place_id_fkey;
 ALTER TABLE lunch_vote_candidates RENAME CONSTRAINT lunch_vote_candidates_restaurant_id_fkey TO lunch_vote_candidates_place_id_fkey;
